02345fe6b3e33a4651fb8ecf0a8154a2eb4dc98c
Unknown <iam@coreylanier.com>
2018-03-08 20:54:52 -0500

    Creation of autogenerator
    Not yet generating HTML based on user response.

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,48 @@
-const ohHey = "Hello World";
+// Create functions object for generator.
 
-console.log(ohHey);
+const streamGenerator = {};
+
+const $iframe = $(`iframe`);
+
+const $window = $(window);
+
+const $dropDownStream = $(`select.stream-type`);
+
+streamGenerator.getIframeYSize = () => {
+  let width = $iframe.width();
+
+  let height = width / 16 * 9;
+
+  streamGenerator.setIframeYSize(height);
+}
+
+streamGenerator.setIframeYSize = (height) => {
+  $iframe.css(`height`, height);
+}
+
+streamGenerator.getIframeFilename = () => {
+  let fileName = $dropDownStream.val();
+
+  streamGenerator.setIframeFilename(fileName);
+}
+
+streamGenerator.setIframeFilename = (fileName) => {
+  console.log(`${fileName}.html`);
+  $iframe.prop(`src`, `${fileName}.html`);
+}
+
+// Change iframe size on document load
+
+$(document).ready(function() {
+  streamGenerator.getIframeYSize();
+});
+
+$window.resize(function() {
+  streamGenerator.getIframeYSize();
+});
+
+// Change the iframe when the drop down changes
+
+$dropDownStream.change(function() {
+  streamGenerator.getIframeFilename();
+});
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,50 @@
 "use strict";
 
-var ohHey = "Hello World";
+// Create functions object for generator.
 
-console.log(ohHey);
+var streamGenerator = {};
+
+var $iframe = $("iframe");
+
+var $window = $(window);
+
+var $dropDownStream = $("select.stream-type");
+
+streamGenerator.getIframeYSize = function () {
+  var width = $iframe.width();
+
+  var height = width / 16 * 9;
+
+  streamGenerator.setIframeYSize(height);
+};
+
+streamGenerator.setIframeYSize = function (height) {
+  $iframe.css("height", height);
+};
+
+streamGenerator.getIframeFilename = function () {
+  var fileName = $dropDownStream.val();
+
+  streamGenerator.setIframeFilename(fileName);
+};
+
+streamGenerator.setIframeFilename = function (fileName) {
+  console.log(fileName + ".html");
+  $iframe.prop("src", fileName + ".html");
+};
+
+// Change iframe size on document load
+
+$(document).ready(function () {
+  streamGenerator.getIframeYSize();
+});
+
+$window.resize(function () {
+  streamGenerator.getIframeYSize();
+});
+
+// Change the iframe when the drop down changes
+
+$dropDownStream.change(function () {
+  streamGenerator.getIframeFilename();
+});
