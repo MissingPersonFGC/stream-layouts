feb8f3d313f967cbf5eb101c8201a708b341b882
Unknown <iam@coreylanier.com>
2018-03-25 18:04:32 -0400

    Finished HD Stream
    Requires Streamlabs

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,5 +1,7 @@
 // Create functions object for generator.
 
+let slideIndex = 0;
+
 const streamGenerator = {};
 
 const $iframe = $(`iframe`);
@@ -7,6 +9,106 @@ const $iframe = $(`iframe`);
 const $window = $(window);
 
 const $dropDownStream = $(`select.stream-type`);
+
+// Create Stream Layout variables
+
+const streamLayout = {};
+
+streamLayout.getWebcamSize = () => {
+  let width = $(`.webcam-estate`).width();
+  let height = width / 16 * 9;
+  console.log(width)
+
+  streamLayout.setWebcamSize(height);
+}
+
+streamLayout.setWebcamSize = (height) => {
+  $(`.webcam-estate`).css(`height`, height);
+}
+
+streamLayout.getHDCaptureSize = () => {
+  let width = $(`.game-capture-estate-hd`).width();
+  let height = width / 16 * 9;
+
+  streamLayout.setHDCaptureSize(height);
+}
+
+streamLayout.getFollower = () => {
+  setInterval(function() {
+    $.ajax({
+      url: `variables/most_recent_follower.txt`,
+      method: `GET`
+    }).then(function(res) {
+      streamLayout.setFollow(res);
+    });
+  }, 10000);
+}
+
+streamLayout.setFollow = (follow) => {
+  $(`.last-follow span`).html(follow);
+}
+
+streamLayout.getSub = () => {
+  setInterval(function() {
+    $.ajax({
+      url: `variables/most_recent_subscriber.txt`,
+      method: `GET`
+    }).then(function(res) {
+      streamLayout.setSub(res);
+    });
+  }, 10000);
+}
+
+streamLayout.showSlides = () => {
+  let slides = $(`.title-slides`);
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = `none`;
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000);
+}
+
+streamLayout.setSub = (sub) => {
+  $(`.last-sub span`).html(sub);
+}
+
+streamLayout.getLastDonor = () => {
+  setInterval(function() {
+    $.ajax({
+      url: `variables/most_recent_donator.txt`,
+      method: `GET`
+    }).then(function(res) {
+      streamLayout.setLastDonor(res);
+    });
+  }, 10000);
+}
+
+streamLayout.setLastDonor = (donor) => {
+  $(`.last-donor span`).html(donor);
+}
+
+streamLayout.getBiggestDonor = () => {
+  setInterval(function() {
+    $.ajax({
+      url: `variables/all_time_top_donator.txt`,
+      method: `GET`
+    }).then(function(res) {
+      streamLayout.setBiggestDonor(res);
+    });
+  }, 10000);
+}
+
+streamLayout.setBiggestDonor = (donor) => {
+  $(`.top-donor span`).html(donor);
+}
+
+streamLayout.setHDCaptureSize = (height) => {
+  $(`.game-capture-estate-hd`).css(`height`, height);
+}
 
 streamGenerator.getIframeYSize = () => {
   let width = $iframe.width();
@@ -27,14 +129,20 @@ streamGenerator.getIframeFilename = () => {
 }
 
 streamGenerator.setIframeFilename = (fileName) => {
-  console.log(`${fileName}.html`);
   $iframe.prop(`src`, `${fileName}.html`);
 }
 
 // Change iframe size on document load
 
-$(document).ready(function() {
+$(function() {
   streamGenerator.getIframeYSize();
+  streamLayout.getHDCaptureSize();
+  streamLayout.getFollower();
+  streamLayout.getSub();
+  streamLayout.getLastDonor();
+  streamLayout.getBiggestDonor();
+  streamLayout.getWebcamSize();
+  setInterval(streamLayout.showSlides, 5000);
 });
 
 $window.resize(function() {
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,6 +2,8 @@
 
 // Create functions object for generator.
 
+var slideIndex = 0;
+
 var streamGenerator = {};
 
 var $iframe = $("iframe");
@@ -9,6 +11,106 @@ var $iframe = $("iframe");
 var $window = $(window);
 
 var $dropDownStream = $("select.stream-type");
+
+// Create Stream Layout variables
+
+var streamLayout = {};
+
+streamLayout.getWebcamSize = function () {
+  var width = $(".webcam-estate").width();
+  var height = width / 16 * 9;
+  console.log(width);
+
+  streamLayout.setWebcamSize(height);
+};
+
+streamLayout.setWebcamSize = function (height) {
+  $(".webcam-estate").css("height", height);
+};
+
+streamLayout.getHDCaptureSize = function () {
+  var width = $(".game-capture-estate-hd").width();
+  var height = width / 16 * 9;
+
+  streamLayout.setHDCaptureSize(height);
+};
+
+streamLayout.getFollower = function () {
+  setInterval(function () {
+    $.ajax({
+      url: "variables/most_recent_follower.txt",
+      method: "GET"
+    }).then(function (res) {
+      streamLayout.setFollow(res);
+    });
+  }, 10000);
+};
+
+streamLayout.setFollow = function (follow) {
+  $(".last-follow span").html(follow);
+};
+
+streamLayout.getSub = function () {
+  setInterval(function () {
+    $.ajax({
+      url: "variables/most_recent_subscriber.txt",
+      method: "GET"
+    }).then(function (res) {
+      streamLayout.setSub(res);
+    });
+  }, 10000);
+};
+
+streamLayout.showSlides = function () {
+  var slides = $(".title-slides");
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 5000);
+};
+
+streamLayout.setSub = function (sub) {
+  $(".last-sub span").html(sub);
+};
+
+streamLayout.getLastDonor = function () {
+  setInterval(function () {
+    $.ajax({
+      url: "variables/most_recent_donator.txt",
+      method: "GET"
+    }).then(function (res) {
+      streamLayout.setLastDonor(res);
+    });
+  }, 10000);
+};
+
+streamLayout.setLastDonor = function (donor) {
+  $(".last-donor span").html(donor);
+};
+
+streamLayout.getBiggestDonor = function () {
+  setInterval(function () {
+    $.ajax({
+      url: "variables/all_time_top_donator.txt",
+      method: "GET"
+    }).then(function (res) {
+      streamLayout.setBiggestDonor(res);
+    });
+  }, 10000);
+};
+
+streamLayout.setBiggestDonor = function (donor) {
+  $(".top-donor span").html(donor);
+};
+
+streamLayout.setHDCaptureSize = function (height) {
+  $(".game-capture-estate-hd").css("height", height);
+};
 
 streamGenerator.getIframeYSize = function () {
   var width = $iframe.width();
@@ -29,14 +131,20 @@ streamGenerator.getIframeFilename = function () {
 };
 
 streamGenerator.setIframeFilename = function (fileName) {
-  console.log(fileName + ".html");
   $iframe.prop("src", fileName + ".html");
 };
 
 // Change iframe size on document load
 
-$(document).ready(function () {
+$(function () {
   streamGenerator.getIframeYSize();
+  streamLayout.getHDCaptureSize();
+  streamLayout.getFollower();
+  streamLayout.getSub();
+  streamLayout.getLastDonor();
+  streamLayout.getBiggestDonor();
+  streamLayout.getWebcamSize();
+  setInterval(streamLayout.showSlides, 5000);
 });
 
 $window.resize(function () {
